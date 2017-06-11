4968794679fc264054d4db626a91ac9e5a7b8b37
Chengyang Chen <chen.chengy@husky.neu.edu>
2017-06-11 15:38:43 -0400

    fix heroku database config

--- assignment/app.js
+++ assignment/app.js
@@ -1,11 +1,18 @@
 
 var mongoose = require('mongoose');
 
+var connectionString = 'mongodb://127.0.0.1:27017/chen-chengyang-webdev';
 if(process.env.MLAB_USERNAME) {
-    var connectionString = 'mongodb://' +
-        process.env.MLAB_USERNAME + ":" +
+    connectionString = process.env.MLAB_USERNAME + ":" +
         process.env.MLAB_PASSWORD + "@ds137281.mlab.com:37281/heroku_2m54jxsb";
 }
+
+
+// if(process.env.MLAB_USERNAME) {
+//     var connectionString = 'mongodb://' +
+//         process.env.MLAB_USERNAME + ":" +
+//         process.env.MLAB_PASSWORD + "@ds137281.mlab.com:37281/heroku_2m54jxsb";
+// }
 
 mongoose.connect(connectionString);
 mongoose.Promise = require('q').Promise;
@@ -13,12 +20,7 @@ mongoose.Promise = require('q').Promise;
 
 
 
-// // var connectionString = 'mongodb://127.0.0.1:27017/test';
-// // var connectionString = 'mongodb://localhost/chen-chengyang-webdev';
-// if(process.env.MLAB_USERNAME) {
-//     var connectionString = 'mongodb://'+ process.env.MLAB_USERNAME + ":" +
-//         process.env.MLAB_PASSWORD + "@ds137281.mlab.com:37281/heroku_2m54jxsb";
-// }
+
 
 require('./services/user.service.server');
 require('./services/website.service.server');
