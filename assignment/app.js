f01d764b75930abc8b905d0f87d08094a37d3d10
Chengyang Chen <chen.chengy@husky.neu.edu>
2017-06-11 15:19:46 -0400

    fix heroku database config

--- assignment/app.js
+++ assignment/app.js
@@ -1,5 +1,12 @@
+var connectionString = 'mongodb://127.0.0.1:27017/test';
+// var connectionString = 'mongodb://localhost/chen-chengyang-webdev';
+if(process.env.MLAB_USERNAME) {
+    connectionString = process.env.MLAB_USERNAME + ":" +
+        process.env.MLAB_PASSWORD + "@ds137281.mlab.com:37281/heroku_2m54jxsb";
+}
+
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/chen-chengyang-webdev');
+mongoose.connect(connectionString);
 mongoose.Promise = require('q').Promise;
 
 require('./services/user.service.server');
@@ -7,3 +14,4 @@ require('./services/website.service.server');
 require('./services/page.service.server');
 require('./services/widget.service.server');
 require('./services/map.service.server');
+
